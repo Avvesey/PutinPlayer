349e4a61da09868208e7fa9fb5ff4b872a5773c0
Avvesey <43779214+Avvesey@users.noreply.github.com>
2019-05-22 17:04:27 +0300

    Update SubBass.cs

--- SubBass.cs
+++ SubBass.cs
@@ -1,122 +1,109 @@
-
-using System.ComponentModel;
-using System.Windows.Forms;
+using System;
+using Un4seen.Bass;
 
 
 namespace WindowsFormsPlayer
 {
-    public partial class Form1 : Form
+    public static class SubBass
     {
-        public Form1()
+        // частота дискретизации
+        private static int hz = 44100;
+
+        //состояние инициализации
+        public static bool InitDefaultDevice;
+
+        // канал
+        public static int Streem;
+
+        // громкость 
+        public static int Volume = 100;
+
+       public static bool check = false;
+
+        // методы
+        //инициализация библеотеки
+        private static bool InitBass(int hz)
         {
-            InitializeComponent();
-        }
+            if (!InitDefaultDevice)
 
-        
-        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
-        {
-            Global_Data.Files.Add(openFileDialog1.FileName);
-            playlist.Items.Add(Global_Data.GetFileName(openFileDialog1.FileName));
-
-        }
+                InitDefaultDevice = Bass.BASS_Init(-1, hz, BASSInit.BASS_DEVICE_DEFAULT, IntPtr.Zero);
+            return InitDefaultDevice;
 
 
-        private void timer1_Tick(object sender, EventArgs e)
-        {
-            label1.Text = TimeSpan.FromSeconds(SubBass.GetPositionOfStream(SubBass.Streem)).ToString();
-            SliderTime.Value = SubBass.GetPositionOfStream(SubBass.Streem);
-        }
 
-        
-        private void Form1_Load(object sender, EventArgs e)
-        {
 
-        }
-
-    
-        private void open_Click(object sender, EventArgs e)
-        {
-            openFileDialog1.ShowDialog();
         }
 
 
-        private void stop_Click(object sender, EventArgs e)
+        //play
+        public static void PLay(string file, int volume)
         {
-            SubBass.check = false;
-            SubBass.Stop();
-            timer1.Enabled = false;
-            SliderTime.Value = 0;
-            label1.Text = "00:00";
-            label2.Text = "00:00";
-        }
-
-
-        private void pause_Click_1(object sender, EventArgs e)
-        {
-            SubBass.check = true;
-            SubBass.Pause();
-            timer1.Enabled = false;
-            SliderTime.Value = SubBass.GetPositionOfStream(SubBass.Streem);
-            label1.Text = TimeSpan.FromSeconds(SubBass.GetPositionOfStream(SubBass.Streem)).ToString();
-        }
-
-
-        private void button1_Click_1(object sender, EventArgs e)
-        {
-            if (SubBass.check == true)
+            
+            Stop();
+            check = true;
+            if (InitBass(hz))
             {
-                SubBass.Chanel_play();
-                timer1.Enabled = true;
-                SliderTime.Value = SubBass.GetPositionOfStream(SubBass.Streem);
-                label1.Text = TimeSpan.FromSeconds(SubBass.GetPositionOfStream(SubBass.Streem)).ToString();
-            }
-            else
-            {
-                string current = Global_Data.Files[playlist.SelectedIndex = 0];
-                SubBass.PLay(current, SubBass.Volume);
-                label1.Text = TimeSpan.FromSeconds(SubBass.GetPositionOfStream(SubBass.Streem)).ToString();
-                label2.Text = TimeSpan.FromSeconds(SubBass.GetTimeOfTrackSteam(SubBass.Streem)).ToString();
-                SliderTime.Maximum = SubBass.GetTimeOfTrackSteam(SubBass.Streem);
-                SliderTime.Value = SubBass.GetPositionOfStream(SubBass.Streem);
-                timer1.Enabled = true;
+                Streem = Bass.BASS_StreamCreateFile(file, 0, 0, BASSFlag.BASS_DEFAULT);
+                if (Streem != 0)
+                {
+                    Volume = volume;
+                    Bass.BASS_ChannelSetAttribute(Streem, BASSAttribute.BASS_ATTRIB_VOL, volume / 100);
+                    Bass.BASS_ChannelPlay(Streem, false);
+                }
             }
         }
 
 
-        private void playlist_SelectedIndexChanged(object sender, EventArgs e)
+
+        // стоп
+        public static void Stop()
         {
+            Bass.BASS_ChannelStop(Streem);
+            Bass.BASS_StreamFree(Streem);
+
+        }
+        // уствновка громкости
+        public static void SetVolumeToStream(int stream, int vol)
+        {
+            Volume = vol;
+            Bass.BASS_ChannelSetAttribute(stream, BASSAttribute.BASS_ATTRIB_VOL, vol / 100F);
+        }
+
+        //Длительность трека 
+        public static int GetTimeOfTrackSteam(int stream)
+        {
+            long TimeBites = Bass.BASS_ChannelGetLength(stream);
+            double Time = Bass.BASS_ChannelBytes2Seconds(stream, TimeBites);
+            return (int)Time;
+
 
         }
 
-
-        private void playlist_DoubleClick_1(object sender, EventArgs e)
+        //получение позиции в секундах
+        public static int GetPositionOfStream(int stream)
         {
-            SubBass.check = true;
-            SubBass.Stop();
+            long pos = Bass.BASS_ChannelGetPosition(stream);
+            double posSec = Bass.BASS_ChannelBytes2Seconds(stream, pos);
+            return (int)posSec;
 
-
-
-            string current = Global_Data.Files[playlist.SelectedIndex];
-            SubBass.PLay(current, SubBass.Volume);
-            label1.Text = TimeSpan.FromSeconds(SubBass.GetPositionOfStream(SubBass.Streem)).ToString();
-            label2.Text = TimeSpan.FromSeconds(SubBass.GetTimeOfTrackSteam(SubBass.Streem)).ToString();
-            SliderTime.Maximum = SubBass.GetTimeOfTrackSteam(SubBass.Streem);
-            SliderTime.Value = SubBass.GetPositionOfStream(SubBass.Streem);
-            timer1.Enabled = true;
+        }
+        // перемотка
+        public static void SetPosOfScroll(int stream, int pos)
+        {
+            Bass.BASS_ChannelSetPosition(stream, (double)pos);
         }
 
-
-        private void SliderVolume_Scroll(object sender, ScrollEventArgs e)
+        public static void Pause()
         {
-            SubBass.SetVolumeToStream(SubBass.Streem, SliderVolume.Value);
+            Bass.BASS_ChannelPause(Streem);
         }
 
-
-        private void SliderTime_Scroll_1(object sender, ScrollEventArgs e)
+        public static void Chanel_play()
         {
-            SubBass.SetPosOfScroll(SubBass.Streem, SliderTime.Value);
+            Bass.BASS_ChannelPlay(Streem, false);
         }
 
-        
+      
     }
+
 }
